609db6fab08628f6a0448f1c75d83c6708fcb671
Anastasiya Kornilova <korniluk13@mail.ru>
2015-04-22 10:59:25 +0300

    Comments were added

--- StackCalc/stackCalc.h
+++ StackCalc/stackCalc.h
@@ -3,10 +3,14 @@
 #include <QString>
 #include "stack.h"
 
+///Class that provides stack calculator for postfix expression
 class StackCalc
 {
 public:
+    ///Constructor with define stack for stack calculator
     Calculator(Stack *stack);
+
+    ///Calculating postfix expression from string
     double calculate(const QString &str);
 
 private:
